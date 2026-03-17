d42f305ca7b8349acf6628862257bc26f8f8f185
Gabriel Leal - PC <gabbrielsleal@gmail.com>
2026-03-17 10:44:32 -0300

    feat(domain): Implementa entidade USER

---
+++ src/backend/src/Cashly.Domain/Identity/Entities/User.cs
@@ -0,0 +1,40 @@
+﻿using Cashly.Domain.Identity.ValueObjects;
+using Cashly.Domain.Shared.Entities;
+
+namespace Cashly.Domain.Identity.Entities
+{
+    public sealed class User : Entity
+    {
+        public Name Name { get; private set; }
+        public Email Email { get; private set; }
+        public PasswordHash PasswordHash { get; private set; }
+        public DateTimeOffset CreatedAt { get; private set; }
+        public DateTimeOffset UpdatedAt { get; private set; }
+
+        private User() { }
+
+        private User(Name name, Email email, PasswordHash passwordHash)
+        {
+            Name = name;
+            Email = email;
+            PasswordHash = passwordHash;
+            CreatedAt = DateTimeOffset.UtcNow;
+        }
+
+        public static User Create(Name name, Email email, PasswordHash password)
+            => new User(name, email, password);
+
+        public void UpdateName(Name name)
+        {
+            Name = name;
+            UpdatedAt = DateTimeOffset.UtcNow;
+        }
+
+        public void UpdateEmail(Email email)
+        {
+            Email = email;
+            UpdatedAt = DateTimeOffset.UtcNow;
+        }
+
+    }
+}
