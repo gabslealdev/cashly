7149e0ea1bf0ab4d308a5b39f57358c28934fb5f
Gabriel Leal <114974022+gabslealdev@users.noreply.github.com>
2026-03-27 18:15:37 -0300

    fix: Corrige bug no result pattern (#57)

--- src/backend/src/Cashly.Application/Shared/Results/Result.cs
+++ src/backend/src/Cashly.Application/Shared/Results/Result.cs
@@ -8,7 +8,7 @@
 
         protected Result(bool isSuccess, Error error)
         {
-            if (isSuccess && error == Error.None)
+            if (isSuccess && error != Error.None)
                 throw new InvalidOperationException("A successful result cannot have an error.");
 
             if (!isSuccess && error == Error.None)
