26bc8f44248611b1aa77f2086cc9cad1d1ea140c
Gabriel Leal - PC <gabbrielsleal@gmail.com>
2026-03-28 07:34:22 -0300

    feat(application): Adiciona LoginUserResponse à camada de aplicação.

---
+++ src/backend/src/Cashly.Application/Identity/UseCases/loginUser/LoginUserResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Cashly.Application.Identity.UseCases.loginUser
+{
+    public sealed class LoginUserResponse
+    {
+        public string AccessToken { get; init; } = string.Empty;
+
+        public DateTime ExpiresAt { get; init; }
+    }
+}
