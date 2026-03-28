32756e48963fc19cdda919f911c6d7aaabc58729
Gabriel Leal - PC <gabbrielsleal@gmail.com>
2026-03-28 07:34:05 -0300

    feat(application): Adiciona LoginUserCommandValidator à camada de aplicação.

---
+++ src/backend/src/Cashly.Application/Identity/UseCases/loginUser/LoginUserCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace Cashly.Application.Identity.UseCases.loginUser
+{
+    public class LoginUserCommandValidator : AbstractValidator<LoginUserCommand>
+    {
+        public LoginUserCommandValidator()
+        {
+            RuleFor(u => u.Email)
+                .NotEmpty().WithMessage("Email is required.")
+                .EmailAddress().WithMessage("Invalid email.");
+
+            RuleFor(u => u.Password)
+                .NotEmpty().WithMessage("Password is required.");
+
+        }
+    }
+}
