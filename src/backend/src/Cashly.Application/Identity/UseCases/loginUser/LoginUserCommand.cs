52222fc2c0b416d7f9522b9be946f93c72f4792c
Gabriel Leal - PC <gabbrielsleal@gmail.com>
2026-03-28 07:32:53 -0300

    feat(application): Adiciona LoginUserCommand à camada de aplicação.

---
+++ src/backend/src/Cashly.Application/Identity/UseCases/loginUser/LoginUserCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace Cashly.Application.Identity.UseCases.loginUser
+{
+    public sealed record LoginUserCommand
+    {
+        public string Email { get; init; } = string.Empty;
+        public string Password { get; init; } = string.Empty;
+    }
+}
